397e0a0a410483d1df2fe6a0c8895b8f1dbc5c4a
sa1nter31 <167094503+sa1nter31@users.noreply.github.com>
2024-04-19 17:05:28 +0300

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,15 @@
+from math import *
+y=2
+z=2
+r1=z**(5*y)
+print('r1=',r1)
+x=0.5
+r2=(sqrt(e**(sin(x))+1))-cos(x/3)**3
+print('r2=',r2)
+x=1
+y=1.5
+r3=(5.2*x)/2*(abs(y))- 4*((log(x))**2)/5*(tan(x))
+print('r3=',r3)
+x=0.3
+r4=(atan(sqrt(x+1))**3)/(x+1.3)+3**x
+print('r4=',r4)
